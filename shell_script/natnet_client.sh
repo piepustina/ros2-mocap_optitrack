9857f262e80893e22b8ccdc1eb6a519bb58cb6a9
Pietro Pustina <39011004+piepustina@users.noreply.github.com>
2024-05-27 14:26:01 +0200

    Update natnet_client.sh

--- shell_script/natnet_client.sh
+++ shell_script/natnet_client.sh
@@ -11,6 +11,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # Move to the correct folder
 cd ${SCRIPT_DIR%shell_script}
 
+# Remove the old build folder
+# rm -r build
+
 # Build the ROS2 package
 echo "Building the package..."
 colcon build --packages-select mocap_optitrack_client mocap_optitrack_interfaces
